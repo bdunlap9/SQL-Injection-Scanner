4ee3f60713ecf646c08a31bd18983c05bf69cd64
Weeke <baileydunlap9@gmail.com>
2020-04-08 16:39:43 -0700

    Update sql.py

--- sql.py
+++ sql.py
@@ -19,8 +19,18 @@ def Main(test, get_database_type):
             print('Site is not vulnerable!')
     elif args.get_database_type:
         urls = [args.get_database_type + "'", args.get_database_type + '"', args.get_database_type[:-4] + ';', args.get_database_type[:-4] + ")", args.get_database_type[:-4] + "')", args.get_database_type[:-4] + '")', args.get_database_type[:-4] + '*']
+        
+        # Database Identifiers
         MySQL_list = ['MySQL', 'MySQL Query fail:', 'SQL syntax', 'You have an error in your SQL syntax', 'mssql_query()', 'mssql_num_rows()']
         PostGre_list = ['']
+        Microsoft_SQL_list = ['']
+        Oracle_list = ['']
+        Advantage_Database_list = ['']
+        Firebird_list = ['']
+        Azure_list = ['']
+        SqlCe_list = ['']
+        VistaDb_list = ['']
+
         for url in urls:
             results = requests.get(url)
             data = results.text
@@ -28,15 +38,48 @@ def Main(test, get_database_type):
             for dbi in MySQL_list:
                 if dbi in MySQL_list:
                     MySQL = True
-                else:
-                    unknown = True
             for dbi in PostGre_list:
                 if dbi in PostGre_list:
                     PostGre = True
+            for dbi in Microsoft_SQL_list:
+                if dbi in Microsoft_SQL_list:
+                    Microsoft_SQL = True
+            for dbi in Oracle_list:
+                if dbi in Oracle_list:
+                    Oracle = True
+            for dbi in Advantage_Database_list:
+                if dbi in Advantage_Database_list:
+                    Advantage_Database_Server = True
+            for dbi in Firebird_list:
+                if dbi in Firebird_list:
+                    Firebird = True
+            for dbi in Azure_list:
+                if dbi in Azure_list:
+                    Azure = True
+            for dbi in SqlCe_list:
+                if dbi in SqlCe_list:
+                    SqlCe = True
+            for dbi in VistaDb_list:
+                if dbi in VistaDb_list:
+                    VistaDb = True
         if MySQL:
             print('Database type is: MySQL')
         elif PostGre:
             print('Database type is: PostGre')
+        elif Microsoft_SQL:
+            print('Database type is: Microsoft SQL Server')
+        elif Oracle:
+            print('Database type is: Oracle')
+        elif Advantage_Database_Server:
+            print('Database type is: Advantage Database Server')
+        elif Firebird:
+            print('Database type is: Firebird')
+        elif Azure:
+            print('Database type is: Azure')
+        elif SqlCe:
+            print('Database type is: SqlCe')
+        elif VistaDb:
+            print('Database type is: VistaDb')
         else:
             print('Database type is: Unknown')
     else:
