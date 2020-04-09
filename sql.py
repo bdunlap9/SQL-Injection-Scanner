70d60f2f51f150cec4e2ec75d3510507ddfca89f
Weeke <baileydunlap9@gmail.com>
2020-04-08 17:14:23 -0700

    Update sql.py

--- sql.py
+++ sql.py
@@ -5,17 +5,18 @@ def Main(test, get_database_type):
     if args.test:
         urls = [args.test + "'", args.test + '"', args.test[:-4] + ';', args.test[:-4] + ")", args.test[:-4] + "')", args.test[:-4] + '")', args.test[:-4] + '*'] 
         vulnerable_text = ['MySQL Query fail:', '/www/htdocs/', 'Query failed', 'mysqli_fetch_array()', 'mysqli_result', 'Warning: ', 'MySQL server', 'SQL syntax', 'You have an error in your SQL syntax;', 'mssql_query()', "Incorrect syntax near '='", 'mssql_num_rows()', 'Notice: ']
-        for url in urls:
-            results = requests.get(url)
-            data = results.text
-            soup = BS(data, features='html.parser')
-            for vuln in vulnerable_text:
-                if vuln in data:
-                    string = vuln
-                    vulnerable = True
-        if vulnerable:
-            print('Site is vulnerable!')
-        else:
+        try:
+            for url in urls:
+                results = requests.get(url)
+                data = results.text
+                soup = BS(data, features='html.parser')
+                for vuln in vulnerable_text:
+                    if vuln in data:
+                        string = vuln
+                        vulnerable = True
+            if vulnerable:
+                print('Site is vulnerable!')
+        except:
             print('Site is not vulnerable!')
     elif args.get_database_type:
         urls = [args.get_database_type + "'", args.get_database_type + '"', args.get_database_type[:-4] + ';', args.get_database_type[:-4] + ")", args.get_database_type[:-4] + "')", args.get_database_type[:-4] + '")', args.get_database_type[:-4] + '*']
